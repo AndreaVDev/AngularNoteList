34aec310da634f3dfcd6e2a45a31660aa36eeaa4
Andrea Verdura <andrea.verdura3@gmail.com>
2021-10-20 16:51:52 +0200

    Added fake notes and material design. Fix layout

--- NoteList/src/app/app.module.ts
+++ NoteList/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+	MatListModule,
+	MatCardModule
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ NoteList/src/app/mock-notes.ts
@@ -0,0 +1,14 @@
+import { Note } from './note';
+
+export const NOTES: Note[] = [
+	{id: 0, title: 'Grocery', description: 'Do some grocery'},
+	{id: 1, title: 'Order', description: 'Buy new pc from Amazon'},
+	{id: 2, title: 'Dinner', description: 'Organize soccer dinner'},
+	{id: 3, title: 'Bowling', description: 'Playing some bowling'},
+	{id: 4, title: 'Date', description: 'Date with Else'},
+	{id: 5, title: 'Music', description: 'Buy tickets for concert'},
+	{id: 6, title: 'Video', description: 'Create video of summer 2021'},
+	{id: 7, title: 'Photo', description: 'Print photost of summer 2021'},
+	{id: 8, title: 'Training', description: 'Lift some weights'},
+	{id: 9, title: 'Doctor', description: 'Do a general checkup'}
+];
