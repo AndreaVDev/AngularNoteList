abf5ca8bf6cce960b3cac99d1b03c0ad375212cc
Andrea Verdura <andrea.verdura3@gmail.com>
2021-10-22 16:34:13 +0200

    Added working input for title

--- NoteList/src/app/app.module.ts
+++ NoteList/src/app/app.module.ts
@@ -7,7 +7,10 @@ import { NotesComponent } from './notes/notes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule } from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     BrowserAnimationsModule,
 	MatListModule,
 	MatCardModule,
+	MatInputModule,
+	MatIconModule,
+	FormsModule,
 	FlexLayoutModule
   ],
   providers: [
